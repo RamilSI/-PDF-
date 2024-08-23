64ca0c1a63395f9112102047ac9c8d349840d0e8
RamilSI <rml701@mail.ru>
2024-08-24 00:34:59 +0300

    directory add

--- files_1.py
+++ files_1.py
@@ -1,40 +1,45 @@
 # программа обхода имен(файлов) в папке и внутри нее
 # импортирую нужные библиотеки
+"""
+входные переменные:
+1. direct - целевая папка где находятся файлы пдф
+2.
+"""
+
+
+
+
 import os
 import re
 import fitz
-import shutil
 
 
-# direct_ = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/07 авг 2024'
-direct = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/07 авг 2024'
-# direct = input('дайте url:  ')
-
-files = os.listdir(direct)[0:7]
+direct = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/2024-06-18 +/'
+files = os.listdir(direct)
 print(files)
 
 file_str = []
-path = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/07 авг 2024/'
-for line in files:
+for i, line in enumerate(files):
     print('Текущее название: ', line)
     if line != '.DS_Store':
-        filename = fitz.open(path + line)
+        filename = fitz.open(direct + line)
         pdf_document = fitz.open(filename)
         text = ''
         for page_num in range(pdf_document.page_count):
             page = pdf_document[page_num]
             text += page.get_text()
-        pdf_document.close()
         match1 = re.findall(r'Партия\s№\s[0-9]+', text)
-
-        print('Новое название: ', ' '.join(match1[0:2]))
-        file_str.append(match1)
-        path_ishod = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/07 авг 2024/' + line
-        print(path_ishod)
-        path_itog = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/07 авг 2024/' + ' '.join(match1[0:2]) + '.pdf'
-        print(path_itog)
+        match2 = re.findall(r'№\s[0-9][0-9][0-9][0-9]', text)
+        pdf_document.close()
+        print('Новое название: ', ' '.join(match2[0:2]))
+        file_str.append(match2)
+        path_old = direct + line
+        print('Старое название', path_old)
+        path_new = direct + ' '.join(match2[0:2]) + '.pdf'
+        print('Новое название', path_old)
+        print('\n', f'------КОНЕЦ {i} страницы -------', '\n')
         try:
-            os.rename(path_ishod, path_itog)
+            os.rename(path_old, path_new)
         except FileNotFoundError:
             print("Ошибка: Файл не найден")
         except PermissionError:
