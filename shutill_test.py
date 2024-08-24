9bbd6bad48e85b6042a8ce59d85eb8472c4ed476
RamilSI <rml701@mail.ru>
2024-08-24 03:51:20 +0300

    init

---
+++ shutill_test.py
@@ -0,0 +1,5 @@
+import shutil
+
+path_current = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/2024-06-18 +/№ 2329.pdf'
+path_new = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/base_sert'
+shutil.move(path_current, path_new)
