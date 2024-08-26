c7065c5aaf3a9264be324c1afdb7be4eb95f4348
RamilSI <rml701@mail.ru>
2024-08-27 00:22:17 +0300

    убрал лишниее

--- files_1_oop.py
+++ files_1_oop.py
@@ -1,4 +1,5 @@
 """
+перевожу код на ООП
 1. передаю путь к целевой папке
 2. сохраняю список файлов
 3. создаю переменную для сохранения текста
@@ -12,42 +13,51 @@
 direct - целевая папка где находятся файлы пдф
 """
 
-import os
-import re
-import fitz
+
+class Sert:
+
+    def __init__(self, patern, direct):  # инициализирую паттерн регулярного выражения и пути к нужному файлу
+        self.direct = direct
+        self.patern = patern
+
+    def new_name(self):
+        import re
+        import fitz
+        import os
+        for i, line in enumerate(os.listdir(self.direct)):
+            if line != '.DS_Store':
+                pdf_document = fitz.open(direct + line)
+                text = ''
+                for page_num in range(pdf_document.page_count):
+                    page = pdf_document[page_num]
+                    if page.get_text() != '':
+                        text += page.get_text()
+                    else:
+                        print('текста нет на странице')
+                match = re.findall(r'№\s[0-9][0-9][0-9][0-9]', text)
+                pdf_document.close()
+                path_old = direct + line
+                print('Старое название', path_old)
+                path_new = direct + ' '.join(match) + '.pdf'
+                print('Новое название', path_new)
+                print('\n', f'------КОНЕЦ {i} страницы -------', '\n')
+                try:
+                    os.rename(path_old, path_new)
+                except FileNotFoundError:
+                  print("Ошибка: Файл не найден")
+                except PermissionError:
+                    print("Нет доступа для переименования файла")
+
+    def list_sert(self):
+        import os
+        for i, name in enumerate(os.listdir(self.direct)):
+            print(i, name)
 
 
-direct = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/07.08.24/'
-files = os.listdir(direct)
-print(files)
-for i, line in enumerate(files):
-    print('Текущее название: ', line)
-    if line != '.DS_Store':
-        pdf_document = fitz.open(direct + line)
-        text = ''
-        for page_num in range(pdf_document.page_count):
-            page = pdf_document[page_num]
-            if page.get_text() != '':
-                text += page.get_text()
-                print('на странице есть текст: ')
-            else:
-                print('текста нет на странице')
-        match = re.findall(r'№\s[0-9][0-9][0-9][0-9]', text)
-        pdf_document.close()
-        print(f'Список имен: {match}')
-        print('Новое название: ', ' '.join(match))
-        path_old = direct + line
-        print('Старое название', path_old)
-        path_new = direct + ' '.join(match) + '.pdf'
-        print('Новое название', path_new)
-        print('\n', f'------КОНЕЦ {i} страницы -------', '\n')
-        try:
-            os.rename(path_old, path_new)
-        except FileNotFoundError:
-            print("Ошибка: Файл не найден")
-        except PermissionError:
-            print("Нет доступа для переименования файла")
+if __name__ == '__main__':
+    pattern = 'Партия\s№\s[0-9]+'
+    direct = '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/07.08.24/'
+    sert_1 = Sert(pattern, direct)
 
-
-
-
+    sert_1.new_name()
+    sert_1.list_sert()
