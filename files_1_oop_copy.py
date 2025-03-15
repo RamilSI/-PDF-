a6cdc90e229153e1a21c76d1606131c8f9220da0
RamilSI <rml701@mail.ru>
2025-03-15 23:18:17 +0300

    ооп через ии квен

--- files_1_oop_copy.py
+++ files_1_oop_copy.py
@@ -23,6 +23,7 @@ class FileHandler:
     def __init__(self, root_path, dir_path):
         self.root_path = os.path.join(root_path, dir_path)
         self.files = self._get_filtered_files()
+        print(self.files)
 
     def _get_filtered_files(self):
         """ Фильтр исключающий файл .DS_Store"""
@@ -37,6 +38,7 @@ class PDFProcessor:
     def extract_text(self, file_path) -> list:
         """Извлекает текст из PDF"""
         with fitz.open(file_path) as doc:
+            print(doc)
             return "".join(page.get_text for page in doc if page.get_text())
 
     def find_matches(self, text):
@@ -58,88 +60,40 @@ class FileRename:
             print(f'Ошибка {e}')
 
 
+config = {
+    'root_path': '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/',
+    'dir_path' : 'Новая папка/',
+    'pattern' :  r'№\s[0-9][0-9][0-9][0-9]|Цвет\sRAL\s\d+|№\s\d+[/]\d+',
+    'dest_dir' : '/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/base_sert'
+}
 
-class Sert:
-    def __init__(self, root_path='/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/',
-                 dir_path='',
-                 path_new='/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/base_sert',
-                 pattern=r'№\s[0-9][0-9][0-9][0-9]|Цвет\sRAL\s\d+|№\s\d+[/]\d+'):  # паттерн регулярного выражения и пути к нужному файлу
-        self.root_path = root_path
-        self.dir_path = dir_path
-        self.pattern = pattern
-        self.path_new = path_new
-        self.__direct = root_path + dir_path
-        self.files = os.listdir(root_path + dir_path)
-        for i, name in enumerate(os.listdir(self.__direct)):
-            print(i, name)
-        print("список файлов: ", os.listdir(self.root_path + self.dir_path))
-        print('длина списка', len(self.files))
 
-    def sanitize_filename(self, name):
-        # Заменяем запрещённые символы на подчёркивания
-        cleaned = re.sub(r'[\\/*?:"<>|]', '_', name)
-        # Удаляем начальные/конечные пробелы и точки
-        cleaned = cleaned.strip(' .')
-        # Заменяем множественные пробелы на один
-        cleaned = re.sub(r'\s+', ' ', cleaned)
-        return cleaned
+class SertificateProcessor:
+    def __init__(self, config):
+        self.file_handler = FileHandler(config['root_path'], config['dir_path'])
+        self.pdf_processor = PDFProcessor(config['pattern'])
+        self.rename = FileRename()
+        self.dest_dir = config['dest_dir']
 
-    def new_name(self):
-        for i, line in enumerate(os.listdir(self.__direct)):
-            if line != '.DS_Store':
-                pdf_document = fitz.open(self.__direct + line)
-                text = ''
-                for page_num in range(pdf_document.page_count):
-                    page = pdf_document[page_num]
-                    if page.get_text() != '':
-                        text += page.get_text()
-                    else:
-                        print('текста нет на странице')
-                match = re.findall(self.pattern, text)
-                pdf_document.close()
-                path_old = os.path.join(self.__direct, line)
-                # path_old = self.__direct + line
-                print('Старое название', path_old)
+    def process_files(self):
+        for file_name in self.file_handler.files:
+            src_path = os.path.join(self.file_handler.root_path, file_name)
+            print(f'старый путь: {src_path}')
+        #     извлекаем тексте и ищем совпадения
+            text = self.pdf_processor.extract_text(src_path)
+            print(f' text {text}')
+            matches = self.pdf_processor.find_matches(text)
+            print(f' new name file: {matches}')
 
-                # В методе new_name измените формирование path_new:
-                match_str = ' '.join(match)
-                cleaned_name = self.sanitize_filename(match_str)
-                path_new = os.path.join(self.__direct, f"{cleaned_name}.pdf")
+        #  формируем новое имя
+            clean_name = self.sanitize_filename(' '.join(matches))
+            dest_path = os.path.join(self.dest_dir, f'{clean_name}.pdf')
 
-                # path_new = self.__direct + ' '.join(match) + '.pdf'
-                print('Новое название', path_new)
-                print('\n', f'------КОНЕЦ {i} страницы -------', '\n')
-
-                try:
-                    os.rename(path_old, path_new)
-                    print(self.files)
-                except FileNotFoundError:
-                    print("Ошибка: Файл не найден")
-                except PermissionError:
-                    print("Нет доступа для переименования файла")
-
-    def new_root(self):
-        for i, file in enumerate(os.listdir(self.__direct)):
-            if file != '.DS_Store':
-                path_current = self.root_path + self.dir_path + file
-                if file not in (os.listdir(self.path_new)):
-                    print(f'номер строки: {i} - файл - {file}')
-                    shutil.move(path_current, self.path_new)
-                else:
-                    print('файл уже присутствует!', file)
-            else:
-                print(file)
-                os.remove(self.root_path+self.dir_path+file)
-
-        os.rmdir(self.root_path + self.dir_path)
+        #     переименование
+            self.rename.rename(src_path, dest_path)
 
 
 if __name__ == '__main__':
 
-    # sert_1 = Sert(dir_path='new/')
-    # sert_1.new_name()
-    # sert_1.new_root()
-    a = FileHandler(root_path='/Users/admin/Library/CloudStorage/GoogleDrive-rml7771@gmail.com/Мой диск/scan/',
-                    dir_path='Новая папка/')
-
-    print(a._get_filtered_files())
+    processor = SertificateProcessor(config)
+    processor.process_files()
