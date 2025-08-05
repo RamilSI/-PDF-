6b52d8ab690c06b30fa50235eaedb99397b79584
RamilSI <rml701@mail.ru>
2025-08-06 03:45:49 +0400

    Исправлен .gitignore и добавлены необходимые файлы

--- remove_test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path_to_remove = '/Users/admin/PycharmProjects/files/платежи_кайт/'
-
-
-for i, el in enumerate(os.listdir(path_to_remove)):
-    print(el)
-    os.remove(path_to_remove + el)
